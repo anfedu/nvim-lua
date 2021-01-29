66b17df52a07a5eafac1385fb140aa223a2f8191
anfedu <firdausnurilahmad@gmail.com>
2021-01-29 14:24:54 +0700

    lspinstall

--- lua/lsp/diagnostic.lua
+++ lua/lsp/diagnostic.lua
@@ -1,12 +1,11 @@
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-    signs = true,
-    virtual_text = {spacing = 4, prefix = '❰'},
-    underline = false,
-    update_in_insert = true
-})
-
-vim.fn.sign_define("LspDiagnosticsSignError", {text = " ", texthl = "LspDiagnosticsDefaultError", linehl = "NONE"})
-vim.fn.sign_define("LspDiagnosticsSignWarning", {text = " ", texthl = "LspDiagnosticsDefaultWarning", linehl = "NONE"})
+vim.fn.sign_define("LspDiagnosticsSignError", {text = "", texthl = "LspDiagnosticsDefaultError", linehl = "NONE"})
+vim.fn.sign_define("LspDiagnosticsSignWarning", {text = "", texthl = "LspDiagnosticsDefaultWarning", linehl = "NONE"})
 vim.fn.sign_define("LspDiagnosticsSignInformation",
-                   {text = " ", texthl = "LspDiagnosticsDefaultInformation", linehl = "NONE"})
-vim.fn.sign_define("LspDiagnosticsSignHint", {text = " ", texthl = "LspDiagnosticsDefaultHint", linehl = "NONE"})
+                   {text = "", texthl = "LspDiagnosticsDefaultInformation", linehl = "NONE"})
+vim.fn.sign_define("LspDiagnosticsSignHint", {text = "", texthl = "LspDiagnosticsDefaultHint", linehl = "NONE"})
+-- vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+--     signs = false,
+--     virtual_text = {spacing = 5, prefix = ''},
+--     underline = false,
+--     update_in_insert = false
+-- })
