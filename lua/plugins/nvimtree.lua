122035de874fc7fccf26e72c48605d1f6a72c82a
anfedu <firdausnurilahmad@gmail.com>
2022-01-17 13:23:59 +0700

    error

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -1,21 +1,21 @@
-vim.g.nvim_tree_side = "left"
+--vim.g.nvim_tree_side = "left"
 -- vim.g.nvim_tree_width = 35
-vim.g.nvim_tree_ignore = { "node_modules", ".cache" }
-vim.g.nvim_tree_gitignore = 1
-vim.g.nvim_tree_auto_open = 0
-vim.g.nvim_tree_auto_close = 0
-vim.g.nvim_tree_auto_ignore_ft = {}
-vim.g.nvim_tree_quit_on_open = 0
+--vim.g.nvim_tree_ignore = { "node_modules", ".cache" }
+--vim.g.nvim_tree_gitignore = 1
+--vim.g.nvim_tree_auto_open = 0
+--vim.g.nvim_tree_auto_close = 0
+--vim.g.nvim_tree_auto_ignore_ft = {}
+--vim.g.nvim_tree_quit_on_open = 0
 -- vim.g.nvim_tree_indent_markers = 1
-vim.g.nvim_tree_hide_dotfiles = 1
-vim.g.nvim_tree_highlight_opened_files = 1
-vim.g.nvim_tree_root_folder_modifier = ":~"
-vim.g.nvim_tree_tab_open = 1
-vim.g.nvim_tree_group_empty = 0
-vim.g.nvim_tree_disable_window_picker = 1
-vim.g.nvim_tree_lsp_diagnostics=0
-vim.g.nvim_tree_hijack_cursor = 0
-vim.g.nvim_tree_show_icons = { git = 1, folders = 1, files = 1 }
+--vim.g.nvim_tree_hide_dotfiles = 1
+--vim.g.nvim_tree_highlight_opened_files = 1
+--vim.g.nvim_tree_root_folder_modifier = ":~"
+--vim.g.nvim_tree_tab_open = 1
+--vim.g.nvim_tree_group_empty = 0
+--vim.g.nvim_tree_disable_window_picker = 1
+--vim.g.nvim_tree_lsp_diagnostics=0
+--vim.g.nvim_tree_hijack_cursor = 0
+--vim.g.nvim_tree_show_icons = { git = 1, folders = 1, files = 1 }
 
 require'nvim-tree'.setup {
   disable_netrw       = true,
