7ae427b8c1a52f051a905d102aee6529b466207e
anfedu <firdausnurilahmad@gmail.com>
2022-01-17 13:25:47 +0700

    fix error

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -1,67 +1,28 @@
---vim.g.nvim_tree_side = "left"
+vim.g.nvim_tree_side = "left"
 -- vim.g.nvim_tree_width = 35
---vim.g.nvim_tree_ignore = { "node_modules", ".cache" }
---vim.g.nvim_tree_gitignore = 1
---vim.g.nvim_tree_auto_open = 0
---vim.g.nvim_tree_auto_close = 0
---vim.g.nvim_tree_auto_ignore_ft = {}
---vim.g.nvim_tree_quit_on_open = 0
+vim.g.nvim_tree_ignore = { "node_modules", ".cache" }
+vim.g.nvim_tree_gitignore = 1
+vim.g.nvim_tree_auto_open = 0
+vim.g.nvim_tree_auto_close = 0
+vim.g.nvim_tree_auto_ignore_ft = {}
+vim.g.nvim_tree_quit_on_open = 0
 -- vim.g.nvim_tree_indent_markers = 1
---vim.g.nvim_tree_hide_dotfiles = 1
---vim.g.nvim_tree_highlight_opened_files = 1
---vim.g.nvim_tree_root_folder_modifier = ":~"
---vim.g.nvim_tree_tab_open = 1
---vim.g.nvim_tree_group_empty = 0
---vim.g.nvim_tree_disable_window_picker = 1
---vim.g.nvim_tree_lsp_diagnostics=0
---vim.g.nvim_tree_hijack_cursor = 0
---vim.g.nvim_tree_show_icons = { git = 1, folders = 1, files = 1 }
+vim.g.nvim_tree_hide_dotfiles = 1
+vim.g.nvim_tree_highlight_opened_files = 1
+vim.g.nvim_tree_root_folder_modifier = ":~"
+vim.g.nvim_tree_tab_open = 1
+vim.g.nvim_tree_group_empty = 0
+vim.g.nvim_tree_disable_window_picker = 1
+vim.g.nvim_tree_lsp_diagnostics=0
+vim.g.nvim_tree_hijack_cursor = 0
+vim.g.nvim_tree_show_icons = { git = 1, folders = 1, files = 1 }
 
-require'nvim-tree'.setup {
-  disable_netrw       = true,
-  hijack_netrw        = true,
-  open_on_setup       = false,
-  ignore_ft_on_setup  = {},
-  update_to_buf_dir   = {
-    enable = true,
-    auto_open = true,
-  },
-  auto_close          = false,
-  open_on_tab         = false,
-  hijack_cursor       = false,
-  update_cwd          = false,
-  diagnostics         = {
-    enable = false,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
-    }
-  },
-  update_focused_file = {
-    enable      = false,
-    update_cwd  = false,
-    ignore_list = {}
-  },
-  system_open = {
-    cmd  = nil,
-    args = {}
-  },
-  view = {
-    width = 30,
-    height = 30,
-    side = 'left',
-    auto_resize = false,
-    mappings = {
-      custom_only = false,
-      list = {}
-    }
-  },
-  filters = {
-    dotfiles = true,
-    custom = {}
-  },
+vim.g.nvim_tree_icons = {
+    default = "",
+    symlink = "",
+    git = { unstaged = "✗", staged = "✓", unmerged = "", renamed = "➜", untracked = "★" },
+    folder = { default = " ", open = " " },
 }
 
-
+-- disable word wrap
+vim.cmd("autocmd FileType LuaTree setlocal nowrap")
