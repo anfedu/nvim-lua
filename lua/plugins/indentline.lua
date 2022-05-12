a315e2ab954a7fd48c0e9e9289fa46077a36a74b
anfedu <firdausnurilahmad@gmail.com>
2022-05-12 09:35:16 +0700

    update

--- lua/plugins/indentline.lua
+++ lua/plugins/indentline.lua
@@ -1,8 +1,11 @@
 -- vim.opt.termguicolors = true
+-- <-- coloring space -->
 vim.cmd [[highlight IndentBlanklineIndent1 guibg=#1f1f1f gui=nocombine]]
 vim.cmd [[highlight IndentBlanklineIndent2 guibg=#2a2a2a gui=nocombine]]
+-- <-- coloring space -->
 
 require("indent_blankline").setup {
+	  -- <-- coloring space -->
     char = "",
     char_highlight_list = {
         "IndentBlanklineIndent1",
@@ -13,4 +16,11 @@ require("indent_blankline").setup {
         "IndentBlanklineIndent2",
     },
     show_trailing_blankline_indent = false,
+	  -- <-- coloring space -->
+
+	  -- <-- indent line -->
+    -- space_char_blankline = " ",
+    -- show_current_context = true,
+    -- show_current_context_start = true,
+	  -- <-- indent line -->
 }
